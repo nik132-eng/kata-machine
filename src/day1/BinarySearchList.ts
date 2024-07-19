53b6be6e0b8dc5906cc44c7120fa4bae8d93ff5d
Nikunj Rohit <nikunjrohit10@gmail.com>
2024-07-19 09:12:21 +0530

    search algo

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,11 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let min=0;
+    let max=haystack.length;
+    while(min<max){
+        const mid = Math.floor(min+(max-min)/2);
+        if(haystack[mid]===needle) return true;
+        else if(haystack[mid]<needle) min=mid+1;
+        else max=mid;
+    }
+    return false
 }
